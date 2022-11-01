a5a97f8729313993400e47e646f9dfb93ef3fe06
tienbaovuong <tienbaoams@gmail.com>
2022-11-01 22:18:34 +0700

    Linear Vertical Layout Constraint Horizontal Layout

--- app/src/main/java/com/example/androidweek3/MainActivity.java
+++ app/src/main/java/com/example/androidweek3/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.androidweek3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private TextView counterText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counterText = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        count++;
+        if (counterText != null)
+            counterText.setText(Integer.toString(count));
     }
 }
